27de6fdf7e486fec8a6e12f5afe62d7e0c390809
JPeralV <joseperalvazquez@gmail.com>
2026-09-21 00:11:51 +0200

    maomeno

--- Programación/Unidad 1/Calendario/Calendario/src/App.java
+++ Programación/Unidad 1/Calendario/Calendario/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.printf("\033[106m|%-10s|%-10s|%-10s|%-10s|%-10s|\n", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes");
+        System.out.printf("\033[106m|%-50s|\n", "======================================================");
+        System.out.printf("\033[106m|\033[103m%-10s\033[106m|\033[105m%-10s\033[106m|\033[105m%-10s\033[106m|\033[104m%-10s\033[106m|%-10s\033[106m\033[106m|\n", "IPE1", "EDES", "EDES", "BADAT", "SINFb");
+        System.out.printf("\033[106m|\033[101m%-10s\033[106m|\033[105m%-10s\033[106m|\033[101m%-10s\033[106m|\033[104m%-10s\033[106m|%-10s\033[106m\033[106m|\n", "PROG", "EDES", "PROG", "BADAT", "SINFb");
+        System.out.printf("\033[106m|\033[101m%-10s\033[106m|\033[40m%-10s\033[106m|\033[101m%-10s|\033[103m%-10s|%-10s\033[106m|\n", "PROG", "DIG", "PROG", "IPE1", "SOS");
+        System.out.printf("\033[106m|\033[107m%-10s%-10s%-10s%-10s%-10s%-4s\033[106m|\n", "R", "E", "C","R", "E", "O");
+        System.out.printf("\033[106m|\033[102m%-10s\033[106m|\033[104m%-10s\033[106m|\033[101m%-10s|\033[103m%-10s|%-10s\033[106m|\n", "SINFb", "BADAT", "PROG", "IPE1", "PROG");
+        System.out.printf("\033[106m|\033[102m%-10s\033[106m|\033[104m%-10s\033[106m|\033[104m%-10s|\033[43m%-10s|%-10s\033[106m|\n", "SINFb", "BADAT", "BADAT", "LMSGb", "PROG");
+        System.out.printf("\033[106m|\033[102m%-10s\033[106m|\033[43m%-10s\033[106m|\033[104m%-10s|\033[43m%-10s|%-10s\033[106m|\n", "SINFb", "LMSGb", "BADAT", "LMSGb", "PROG");
+        System.out.printf("\033[106m|%-50s|\n", "======================================================");
+        
+        
     }
 }
