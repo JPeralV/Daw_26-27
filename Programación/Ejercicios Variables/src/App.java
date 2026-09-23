af2102094599dc9d44ae94bff222c4d1a4191d2d
JPeralV <joseperalvazquez@gmail.com>
2026-09-23 11:04:01 +0200

    Iniciados ejercicios variables

---
+++ Programación/Ejercicios Variables/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x=144;
+        int y = 999;
+        float result = (x/y);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(result);
+        System.out.println(x*y);
+    }
+}
